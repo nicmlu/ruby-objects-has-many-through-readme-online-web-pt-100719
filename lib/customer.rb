0b29913ec5ad16acf6af97901087e1cf8d414171
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 21:09:52 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,14 @@
 class Customer
   attr_reader :name, :age 
+  @@all = []
   
   def initialize(name, age)
     @name = name 
     @age = age 
+    @@all.push(self)
   end 
   
+  def self.all
+    @@all
+  end 
 end
